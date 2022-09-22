51bbc922196699adc4bbab3c0c95f59115d82c48
Firas_Alsghier <64198500+Firas-Alsghier@users.noreply.github.com>
2022-09-22 22:46:26 +0200

    Update content

---
+++ dist/index.71e390fd.js
@@ -0,0 +1,2 @@
+let burgerMenu=document.querySelector(".burger_menu"),navigation=document.querySelector(".nav_list");function slideMenu(){burgerMenu.classList.toggle("switch_menu"),navigation.classList.toggle("toggle_navigation")}burgerMenu.addEventListener("click",slideMenu),AOS.init({delay:50});
+//# sourceMappingURL=index.71e390fd.js.map
